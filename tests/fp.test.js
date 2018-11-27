a94f9df2a0e657e4daa9c05572e4e30e65665e45
Fabrizio Ruggeri <fabrizio.ruggeri@gmail.com>
2018-11-27 09:33:38 +0100

    Test passed values

--- tests/fp.test.js
+++ tests/fp.test.js
@@ -1,17 +1,46 @@
-const memoizy = require('../fp');
+const memoizyFP = require('../fp');
+const memoizy = require('../index');
+
+jest.mock('../index', () => jest.fn());
 
 describe('FP memoizy', () => {
+  beforeEach(() => {
+    memoizy.mockClear();
+  });
+
   test('the function is curried', () => {
-    const m = memoizy({});
+    const m = memoizyFP({});
     expect(m).toBeInstanceOf(Function);
   });
 
   test('the memoizer is eventually returned', () => {
     const double = jest.fn(a => a * 2);
-    const m = memoizy({});
-    const memDouble = m(double);
-    memDouble(2);
-    memDouble(2);
-    expect(double).toHaveBeenCalledTimes(1);
+    const m = memoizyFP({});
+    m(double);
+    expect(memoizy).toHaveBeenCalledTimes(1);
+  });
+
+  test('all the options are passed', () => {
+    const double = jest.fn(a => a * 2);
+    const cacheFactory = () => new Map();
+    const cacheKeyBuilder = (...args) => JSON.stringify(args);
+    const valueAcceptor = () => true;
+    const m = memoizyFP({
+      maxAge: 2000,
+      cache: cacheFactory,
+      cacheKey: cacheKeyBuilder,
+      valueAccept: valueAcceptor,
+    });
+    m(double);
+    expect(memoizy).toHaveBeenCalledTimes(1);
+    expect(memoizy).toHaveBeenCalledWith(
+      double,
+      {
+        maxAge: 2000,
+        cache: cacheFactory,
+        cacheKey: cacheKeyBuilder,
+        valueAccept: valueAcceptor,
+      },
+    );
   });
 });
